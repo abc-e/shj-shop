6604d63e0a2e1de279203debcdcda73d77a831a4
lishijiee <2424782003@qq.com>
2023-04-06 20:08:50 +0800

    Initial commit

---
+++ src/main/java/io/shj/boot/ShjShopApplication.java
@@ -0,0 +1,13 @@
+package io.shj.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShjShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShjShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/shj/boot/ShjShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shj.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShjShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
